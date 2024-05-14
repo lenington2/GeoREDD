dc11c3b98fe4dc3b1ebcc3a0591f7de2feb0c3f6
lenington2 <lenington1@gmail.com>
2024-05-14 07:50:05 +0200

    Cambio Tamaño Layout

---
+++ resources/views/components/home.blade.php
@@ -0,0 +1,6 @@
+<div class="p-6 lg:p-8 bg-white border-b border-gray-200">   
+   
+    <iframe id="REDD" title="REDD" src="https://webgis.redd-realestate-data.com/it/map/pratiche_studio_delli_santi/?__bratk=0cac317bb288a3e4fc17f943f34b459f315702c9" width=100% height="600" frameborder="0" allowfullscreen></iframe>
+                                       
+</div>
+
