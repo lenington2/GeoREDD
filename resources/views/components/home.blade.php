502d5c834b0f4c20e97b5e10c20bd86aa7826ce4
Luis26021 <luigiborrello26@gmail.com>
2024-05-16 11:20:30 +0200

    cambio tabella e mailer in .env

--- resources/views/components/home.blade.php
+++ resources/views/components/home.blade.php
@@ -1,6 +1,4 @@
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-
-
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     @if ($errors->any())
         <div class="col-lg-12">
             <div class="alert alert-danger alert-dismissible fade show">
@@ -28,18 +26,14 @@
         </div>
     @endif
     <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
-
-
         <div class="table-responsive">
             <table class="table table-hover" id="dataTable0" width="100%" cellspacing="0">
                 <thead>
                     <tr>
-                        <th>Ordine</th>
-                        <th>Data</th>
-                        <th>Cliente</th>
-                        <th>Ref. Ordine</th>
-
-                        <th style="text-align: center; width: 100px;">Risposta</th>
+                        <th>Utente</th>
+                        <th>Titolo</th>
+                        <th>Data caricamento</th>
+                        <th style="text-align: center; width: 100px;">Azioni</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -48,13 +42,18 @@
                         <td><b></b></td>
                         <td></td>
                         <td></td>
-                        <td></td>
-
-
                         <td style="text-align: center; width: 100px;">
-                            <a style="float: center; font-size: 12px; margin-left: 5px;" class="btn btn-primary btn-sm"
-                                data-toggle="modal" data-target="#response" data-resp="">
+                            <a style="float: center; font-size: 12px; margin-left: 5px;" class="btn btn-success btn-sm"
+                                data-toggle="tooltip" data-target="#response" data-resp="" title="vedi progetto">
                                 <span class="fas fa-eye" aria-hidden="true"></span>
+                            </a>
+                            <a style="float: center; font-size: 12px; margin-left: 5px;" class="btn btn-primary btn-sm"
+                                data-toggle="tooltip" data-target="#response" data-resp="" title="modifica progetto">
+                                <span class="fas fa-edit" aria-hidden="true"></span>
+                            </a>
+                            <a style="float: center; font-size: 12px; margin-left: 5px;" class="btn btn-danger btn-sm"
+                                data-toggle="tooltip" data-target="#response" data-resp="" title="elimina progetto">
+                                <span class="fas fa-trash" aria-hidden="true"></span>
                             </a>
                         </td>
                     </tr>
@@ -63,4 +62,3 @@
             </table>
         </div>
     </div>
-   
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,85 +1,89 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>GeoREDD</title>        
-        <link rel="shortcut icon" type="image/png" href="{{ url('images/mondos.png') }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>GeoREDD</title>
+    <link rel="shortcut icon" type="image/png" href="{{ url('images/mondos.png') }}">
 
-         <!-- Custom fonts for this template-->
-        <link href="contenido/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-        <link
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Custom fonts for this template-->
+    <link href="contenido/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
-        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
-    
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">     
-        <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-        
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
 
-        <!-- Styles -->
-        @livewireStyles
-    </head>
-    <body class="font-sans antialiased">
-        <x-banner />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+    <!-- Styles -->
+    @livewireStyles
+</head>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
+<body class="font-sans antialiased">
+    <x-banner />
 
-        @stack('modals')
+    <div class="min-h-screen bg-gray-100">
+        @livewire('navigation-menu')
 
-        @livewireScripts
-        <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
-        
-    </body>
-    <script>
-         $(document).ready(function() {
+        <!-- Page Heading -->
+        @if (isset($header))
+            <header class="bg-white shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    {{ $header }}
+                </div>
+            </header>
+        @endif
+
+        <!-- Page Content -->
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
+
+    @stack('modals')
+
+    @livewireScripts
+    <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+
+</body>
+<script>
+    $(document).ready(function() {
         var table = $('#dataTable0').DataTable({
             "responsive": true,
             "pageLength": 10,
-            "order": [[1, "asc"]],
+            "order": [
+                [1, "asc"]
+            ],
             "language": {
-               "sEmptyTable": "Nessun dato disponibile nella tabella",
-                    "sInfo": "Visualizzazione da _START_ a _END_ di _TOTAL_ elementi",
-                    "sInfoEmpty": "Visualizzazione da 0 a 0 di 0 elementi",
-                    "sInfoFiltered": "(filtrati da _MAX_ elementi totali)",
-                    "sInfoPostFix": "",
-                    "sInfoThousands": ",",
-                    "sLengthMenu": "Visualizza _MENU_ elementi",
-                    "sLoadingRecords": "Caricamento...",
-                    "sProcessing": "Elaborazione...",
-                    "sSearch": "Ricerca:",
-                    "sZeroRecords": "La ricerca non ha portato alcun risultato.",
-                    "oPaginate": {
-                        "sFirst": "Inizio",
-                        "sPrevious": "Precedente",
-                        "sNext": "Successivo",
-                        "sLast": "Fine"
+                "sEmptyTable": "Nessun dato disponibile nella tabella",
+                "sInfo": "Visualizzazione da _START_ a _END_ di _TOTAL_ elementi",
+                "sInfoEmpty": "Visualizzazione da 0 a 0 di 0 elementi",
+                "sInfoFiltered": "(filtrati da _MAX_ elementi totali)",
+                "sInfoPostFix": "",
+                "sInfoThousands": ",",
+                "sLengthMenu": "Visualizza _MENU_ elementi",
+                "sLoadingRecords": "Caricamento...",
+                "sProcessing": "Elaborazione...",
+                "sSearch": "Ricerca:",
+                "sZeroRecords": "La ricerca non ha portato alcun risultato.",
+                "oPaginate": {
+                    "sFirst": "Inizio",
+                    "sPrevious": "Precedente",
+                    "sNext": "Successivo",
+                    "sLast": "Fine"
                 },
                 "aria": {
                     "sSortAscending": ": attiva per ordinare la colonna in ordine crescente",
@@ -87,6 +91,10 @@
                 }
             }
         });
-    });   
-    </script>
+    });
+    $(document).ready(function() {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+</script>
+
 </html>
