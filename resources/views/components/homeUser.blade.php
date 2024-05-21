80aefdd21558b6d01b9dece90d6aaffe3bcc5c6e
Luis26021 <luigiborrello26@gmail.com>
2024-05-21 17:26:00 +0200

    cambiamento dinamico mappa dashboard User

--- resources/views/components/homeUser.blade.php
+++ resources/views/components/homeUser.blade.php
@@ -1,14 +1,13 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-
 @include('include.delete')
 
 <div class="row">
     <div class="col-3">
         <div class="card-header d-flex justify-content-between align-items-center bg-dark">
-            <h5 class="card-title text-white">Elenco Progetti <i data-feather="info" class="feather-sm fill-white me-1"
-                    data-bs-toggle="tooltip" data-bs-placement="top"
-                    title="In questa sezione puoi gestire i progetti, associarne uno a un cliente e crearne uno nuovo dal pulsante Nuovo"></i>
-            </h5>           
+            <h5 id="card-title" class="card-title text-white">
+                Elenco progetti
+                <i data-feather="info" class="feather-sm fill-white me-1" data-bs-toggle="tooltip" data-bs-placement="top" title="*********"></i>
+            </h5>
         </div>
         <div class="card-body bg-light">
             <div class="table-responsive">
@@ -16,15 +15,15 @@
                     <thead>
                         <tr>
                             <th>Titolo</th>
-                            <th>xxx</th>
+                            <th>Azioni</th>
                         </tr>
                     </thead>
                     <tbody>
                         @foreach ($projects as $project)
-                            <tr>
-                                <td>{{ $project->title }}</td>
-                                <td>xxx</td>
-                            </tr>
+                        <tr>
+                            <td>{{ $project->title }}</td>
+                            <td><button class="btn btn-primary show-map" data-url="{{ $project->url_mappa }}" data-title="{{ $project->title }}" data-id="{{ $project->idproject }}">Mostra Mappa</button></td>
+                        </tr>
                         @endforeach
                     </tbody>
                 </table>
@@ -32,29 +31,42 @@
         </div>
     </div>
     <div class="col-9">
-
         <div class="card" id="fullCard">
             <div class="card-header d-flex justify-content-between align-items-center bg-dark">
-                <h5 class="card-title text-white">{{-- {{ $projects->title }} --}} <i data-feather="info"
-                        class="feather-sm fill-white me-1" data-bs-toggle="tooltip" data-bs-placement="top"
-                        title="*********"></i>
+                <h5 id="project-title" class="card-title text-white">
+                    {{-- {{ $projects->title }} --}}
+                    <i data-feather="info" class="feather-sm fill-white me-1" data-bs-toggle="tooltip" data-bs-placement="top" title="*********"></i>
                 </h5>
                 <div class="dropdown">
-                    <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
-                        Azioni
-                    </button>
+                    <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">Azioni</button>
                     <div class="dropdown-menu">
                         <a class="dropdown-item" href="#" id="fullScreenBtn">FullScreen</a>
-                        <a class="dropdown-item" href="{{-- {{ url('/download/' . $projects->idproject) }} --}}">Scarica file</a>
+                        <a id="download-link" class="dropdown-item" href="#">Scarica file</a>
                     </div>
                 </div>
             </div>
-
             <div class="card-body bg-light" id="cardBody">
-                <iframe id="GeoREDD" title="GeoREDD" style="width: 100%; height: 400" height="600"
-                    src="{{-- {{ $projects->url_mappa }} --}}"> </iframe>
+                <iframe id="GeoREDD" title="GeoREDD" style="width: 100%; height: 400" height="600" src="https://google.com"></iframe>
             </div>
         </div>
-
     </div>
 </div>
+
+<script>
+    $(document).ready(function() {
+        $('.show-map').click(function() {
+            var mapUrl = $(this).data('url');
+            var projectTitle = $(this).data('title');
+            var projectId = $(this).data('id');
+
+            // Update the iframe source
+            $('#GeoREDD').attr('src', mapUrl);
+
+            // Update the card title
+            $('#project-title').text(projectTitle);
+
+            // Update the download link
+            $('#download-link').attr('href', '/download/' + projectId);
+        });
+    });
+</script>
