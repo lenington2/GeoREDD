3fe8028095d6cc31972c4713292742a412ab643d
lenington2 <lenington1@gmail.com>
2024-05-21 13:06:49 +0200

    Layout User Home 2 card

--- resources/views/components/homeUser.blade.php
+++ resources/views/components/homeUser.blade.php
@@ -1,54 +1,60 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
 @include('include.delete')
-<div class="card-header d-flex justify-content-between align-items-center bg-dark">
-    <h5 class="card-title text-white">Elenco Progetti <i data-feather="info" class="feather-sm fill-white me-1"
-            data-bs-toggle="tooltip" data-bs-placement="top"
-            title="In questa sezione puoi gestire i progetti, associarne uno a un cliente e crearne uno nuovo dal pulsante Nuovo"></i>
-    </h5>
-    <a href="{{url('/new-project')}}" class="btn btn-info btn-sm disabled">
-        <span class="fas fa-plus" aria-hidden="true"></span>
-        Nuovo
-    </a>
-</div>
-<div class="card-body bg-light">
-    <div class="table-responsive">
-        <table class="table table-hover" id="dataTable0" width="100%" cellspacing="0">
-            <thead>
-                <tr>
-                    <th>Titolo</th>
-                    <th>Note</th>
-                    <th>Data caricamento</th>
-                    <th>Caricato da</th>
-                    <th style="text-align: center; width: 100px;">Azioni</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($projects as $project)
-                <tr>
-                    <td>{{ $project->title }}</td>
-                    <td>{{ $project->note }}</td>
-                    <td>{{ $project->created_at }}</td>
-                    <td><b>{{ $project->creator_name }}</b></td>
-                    <td style="text-align: center; width: 100px;">
-                        <a href="{{ url('/mappa/' . $project->idproject) }}"
-                            style="float: center; font-size: 12px; margin-left: 5px;" class="btn btn-success btn-sm"
-                            data-toggle="tooltip" title="vedi progetto">
-                            <span class="fas fa-eye" aria-hidden="true"></span>
-                        </a>
-                        <a href="{{ url('/edit-project/' . $project->idproject) }}"
-                            style="float: center; font-size: 12px; margin-left: 5px;" class="btn btn-primary btn-sm disabled"
-                            data-toggle="tooltip" data-target="#response" data-resp="" title="modifica progetto">
-                            <span class="fas fa-edit" aria-hidden="true"></span>
-                        </a>
-                        <a style="float: center; font-size: 12px; margin-left: 5px;" class="btn btn-danger btn-sm disabled"
-                            data-toggle="modal" data-target="#delete" data-idprogetto="{{$project->idproject}}" data-title="{{$project->title}}" title="elimina progetto">
-                            <span class="fas fa-trash" aria-hidden="true"></span>
-                        </a>
-                    </td>
-                </tr>
-                 @endforeach
-            </tbody>
-        </table>
+
+<div class="row">
+    <div class="col-3">
+        <div class="card-header d-flex justify-content-between align-items-center bg-dark">
+            <h5 class="card-title text-white">Elenco Progetti <i data-feather="info" class="feather-sm fill-white me-1"
+                    data-bs-toggle="tooltip" data-bs-placement="top"
+                    title="In questa sezione puoi gestire i progetti, associarne uno a un cliente e crearne uno nuovo dal pulsante Nuovo"></i>
+            </h5>           
+        </div>
+        <div class="card-body bg-light">
+            <div class="table-responsive">
+                <table class="table table-hover" id="dataTable0" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>Titolo</th>
+                            <th>xxx</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($projects as $project)
+                            <tr>
+                                <td>{{ $project->title }}</td>
+                                <td>xxx</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <div class="col-9">
+
+        <div class="card" id="fullCard">
+            <div class="card-header d-flex justify-content-between align-items-center bg-dark">
+                <h5 class="card-title text-white">{{-- {{ $projects->title }} --}} <i data-feather="info"
+                        class="feather-sm fill-white me-1" data-bs-toggle="tooltip" data-bs-placement="top"
+                        title="*********"></i>
+                </h5>
+                <div class="dropdown">
+                    <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+                        Azioni
+                    </button>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#" id="fullScreenBtn">FullScreen</a>
+                        <a class="dropdown-item" href="{{-- {{ url('/download/' . $projects->idproject) }} --}}">Scarica file</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card-body bg-light" id="cardBody">
+                <iframe id="GeoREDD" title="GeoREDD" style="width: 100%; height: 400" height="600"
+                    src="{{-- {{ $projects->url_mappa }} --}}"> </iframe>
+            </div>
+        </div>
+
     </div>
 </div>
