d139c97646d67a395b082a07d690545c96ce26a8
lenington2 <lenington1@gmail.com>
2024-05-24 09:54:39 +0200

    Gestione degli errori del server

---
+++ resources/views/errors/layout.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@yield('title')</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 36px;
+                padding: 20px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+
+                <div class="container">
+                    <div class="jumbotron">
+                        <img src="{{ url('images/GeoREDD_logo_scritta.png') }}" class="rounded-img-thumbnail" alt="Cinque Terre" width=50% height=50%>
+                      <h1>Ops! qualcosa è andato storto</h1>      
+                      <p> <button type="button" style="float: center; margin-right: 10px; margin-top: 1px;" class="btn btn-danger" onclick="history.back(-1);">
+                        <span class="fas fa-arrow-left" aria-hidden="true"></span> Ritorno     
+                        </button></p>
+                        @yield('message') 
+                        <div class="title">
+                    </div>                        
+                  </div>                                  
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
