7161325b6715bec10131b8dededb488918d34547
Luis26021 <luigiborrello26@gmail.com>
2024-05-31 09:38:57 +0200

    aggiunta lingua italiana

---
+++ resources/lang/it/passwords.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | outcome such as failure due to an invalid password / reset token.
+    |
+    */
+
+    'reset' => 'La password è stata reimpostata.',
+    'sent' => 'Abbiamo inviato il link per il reset della password via email.',
+    'throttled' => 'Attendi prima di riprovare.',
+    'token' => 'Questo token di reset della password non è valido.',
+    'user' => "Non riusciamo a trovare un utente con quell'indirizzo email.",
+
+
+];
